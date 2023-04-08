df8764b7118464077698f6c8283bfbef6c382db3
Samkiroko <kirokodev@gmail.com>
2023-04-08 13:57:19 +0300

    generating access token

---
+++ .history/Controllers/HomeController_20230408133223.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using mpesaIntegration.Models;
+
+namespace mpesaIntegration.Controllers;
+
+public class HomeController : Controller
+{
+  private readonly ILogger<HomeController> _logger;
+  private readonly IHttpClientFactory _clientFactory;
+
+  public HomeController(ILogger<HomeController> logger, IHttpClientFactory clientFactory)
+  {
+    _logger = logger;
+    _clientFactory = clientFactory;
+  }
+
+  public IActionResult Index()
+  {
+    return View();
+  }
+
+  public async Task<string> GetToken()
+  {
+    var client = _clientFactory.CreateClient("mpesa");
+  }
+
+  public IActionResult Privacy()
+  {
+    return View();
+  }
+
+  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+  public IActionResult Error()
+  {
+    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+  }
+}
